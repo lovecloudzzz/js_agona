a83bccbe4e8b259427d9d3012a9cabe0bca89b92
lovecloudzzz <lovecloudez@gmail.com>
2022-10-20 19:22:28 +0300

    Работа со строками

--- cleanString.js
+++ cleanString.js
@@ -1,6 +1,6 @@
 // Напишите функцию, которая возвращает строку, очищенную от всех не буквенно-цифровых символов.
 let test = "word ,./ ?; : Word []{} =+-!@#$%^&* 123"
 function cleanString(str) {
-    return str.replace(/\W/g, "");
+    return str.replace(/\W/g, "")
 }
 console.log(cleanString(test));
--- unique.js
+++ unique.js
@@ -1,6 +1,6 @@
 //Напишите функцию, которая возвращает строку, оставив в ней только уникальные символы, т.е. встречающиеся в строке один раз.
 let test = "251wrtu1hdlop2f=-32 fg-2kfopckdalsdj-q9fsduf89sd97sb"
 function unique(str){
-    return(Array.from(new Set(str)).join(''))
+    return Array.from(new Set(str)).join('')
 }
 console.log(unique(test));
