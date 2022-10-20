50ff7dd5717aaaeea37a2b0d02b6987076e0ca56
lovecloudzzz <lovecloudez@gmail.com>
2022-10-20 20:50:44 +0300

    fix

--- palindrome.js
+++ palindrome.js
@@ -1,6 +1,7 @@
 //Напишите функцию, которая проверяет является ли строка палиндромом или нет (возвращает true или false).
-let test = "Anna"
+let test = 'A man, a plan, a canal, Panama!'
 function palindrome(str) {
+    str = str.replace(/\.|\,| |\!|\?|\'|\"|\/|\\|\-|\_|\+|\‘|\’/g, '')
     return str.toLowerCase() === str.split('').reverse().join('').toLowerCase()
 }
 console.log(palindrome(test));
